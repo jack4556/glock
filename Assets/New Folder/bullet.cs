4cece6175a33680f03b893fdb8e0b3db3974f67c
jack4556 <jackdmcpherson455@gmail.com>
2022-02-21 10:53:28 +1100

    fun scripts

---
+++ Assets/New Folder/bullet.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
+        if (playerGO != null)
+        {
+            player = playerGO.transform;
+            target = new Vector2(player.position.x, player.position.y);
+        }
+        else
+            DestroyProjectile();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime, Space.World);
+        if (transform.position.x < -4)
+            DestroyProjectile();
+        /*transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }*/
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerAttack"))
+        {
+            DestroyProjectile();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerAttack"))
+        {
+            DestroyProjectile();
+        }
+    }
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
